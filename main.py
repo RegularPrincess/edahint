679cfae59baa5de06d5a379df88759826e742474
Juriy Fedorov <jmfedorov@gmail.com>
2017-09-08 15:36:29 +0300

    init project

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+import model
+
+
+app = Flask(__name__)
+
+
+@app.route('/get_hint', methods=['POST'])
+def get_hint():
+    json_dict = request.get_json()
+    return jsonify(json_dict)
+
+
+if __name__ == '__main__':
+    # hint = model.compute_hint([' грудк', ' хле', ' яблок', ' сыр ', 'сырок '], 'low')
+    # print(hint)
+    app.run()
+
